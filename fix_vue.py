38516caf028c920a754f56132f4ccc89fd722a46
xand.jpeg <125098050+xandejpeg@users.noreply.github.com>
2026-04-08 17:19:16 -0300

    chore: remove temp fix script

--- fix_vue.py
+++
@@ -1,5 +0,0 @@
-﻿f = '/opt/easy-social/frontend/src/views/AdminPanelView.vue'
-lines = open(f).readlines()
-lines[56] = '          @click="activeTab = tab.id; loadTabData()"\n'
-open(f,'w').writelines(lines)
-print('FIXED')
